a51fc6db4edf3dc15a8cb682a1bb7e447a6d4832
Ariel <127093985+ariel3007@users.noreply.github.com>
2024-06-25 15:31:41 +0300

    initial commit - convert to kotlin

---
+++ src/main/java/me/olios/plugins/donkeyborder/DonkeyBorder.kt
@@ -0,0 +1,13 @@
+package me.olios.plugins.donkeyborder
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class DonkeyBorder : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
